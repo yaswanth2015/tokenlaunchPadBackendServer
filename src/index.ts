9bdb20fcee3b10fd6dde51838a80f35fbca7a2ff
yaswanth <aysawnth@enphaseenergy.com>
2024-09-29 22:27:02 +0530

    added all cors options

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from "express"
 import ConnectTOMongo from "./dbConfiguration/ConnectTOMongo"
 import router from "./routes/UserRoutes"
+import cors from "cors"
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use("/api/user", router)
 ConnectTOMongo.ConnectToDB()
